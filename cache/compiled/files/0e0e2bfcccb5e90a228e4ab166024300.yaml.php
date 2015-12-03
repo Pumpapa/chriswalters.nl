4f2b70a25079322605d1962efc1d9624e796383c
Pum Walters <pum@babelfish.nl>
2015-12-03 22:53:29 +0100

    initial commit

---
+++ cache/compiled/files/0e0e2bfcccb5e90a228e4ab166024300.yaml.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => '/Users/pum/Dropbox/Sloth/Sites/MacWebsitesDocuments/chriswalters.nl/user/config/security.yaml',
+    'modified' => 1449179604,
+    'data' => [
+        'salt' => 'UldZcfoF6mqTin'
+    ]
+];
---
+++ cache/compiled/files/444b6b482e001e308180e7e6f663981a.yaml.php
@@ -0,0 +1,16 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => '/Users/pum/Dropbox/Sloth/Sites/MacWebsitesDocuments/chriswalters.nl/user/config/site.yaml',
+    'modified' => 1449179241,
+    'data' => [
+        'title' => 'Grav',
+        'author' => [
+            'name' => 'Joe Bloggs',
+            'email' => 'joe@test.com'
+        ],
+        'metadata' => [
+            'description' => 'Grav is an easy to use, yet powerful, open source flat-file CMS'
+        ]
+    ]
+];
